060bf3a20e5ade274053cca41189723240811bc5
Raizer58 <Raizer9058w@gmail.com>
2020-02-26 06:59:13 +0300

    init: start typical-arrays-problens task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
+  if(array){
+    if (array.length > 1){
+      return array.sort((a, b) => a-b).splice(0,1);
+    };
+  };
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array){
+    if (array.length > 1){
+      return array.sort((a, b) => b-a).splice(0,1);
+    };
+  };
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array){
+    if (array.length > 1){
+      return array.reduce((acc, arg) => acc+=arg)/array.length;
+    };
+  };
   return 0;
 }
